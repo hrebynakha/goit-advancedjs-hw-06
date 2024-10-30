874208e199b06c97584a024f20052ea9639759fc
Hrebynakha Anatolii <hrebynakha@gmail.com>
2024-10-30 18:45:12 +0200

    add hw

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -8,19 +8,19 @@ type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number
-}
+  weight: number;
+};
 
 function compare<T>(
   top: Pick<AllType, "name" | "color">,
-  bottom: Pick<AllType, "position"| "weight">
+  bottom: Pick<AllType, "position" | "weight">
 ): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
 
 // const result = compare(
--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,12 +2,30 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component<T> {
-  constructor (public props:T) {}
+// class Component {
+//   constructor (public props:T) {
+
+//   }
+// }
+
+// class Page extends Component {
+//   pageInfo () {
+//     console.log(this.props.title);
+//   }
+// }
+
+interface ICompnent<T> {
+  props: {
+    title: T;
+  };
+}
+
+class Component<T> implements ICompnent<T> {
+  constructor(public props: T) {}
 }
 
 class Page<T> extends Component<T> {
-  pageInfo () {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
